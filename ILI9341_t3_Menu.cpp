0d53e1bb45025aa024d108183860caf41b626c40
Kris Kasprzak <kris.kasprzak@yahoo.com>
2025-01-27 15:23:33 -0600

    Update ILI9341_t3_Menu.cpp

--- ILI9341_t3_Menu.cpp
+++ ILI9341_t3_Menu.cpp
@@ -794,7 +794,7 @@ void EditMenu::setItemColors( uint16_t DisableTextColor, uint16_t HighlightBorde
 	sbcolor = SelectBorderColor;
 }
 
-void EditMenu::SetAllColors(uint16_t TextColor, uint16_t BackgroundColor, 
+void EditMenu::setAllColors(uint16_t TextColor, uint16_t BackgroundColor, 
 							uint16_t HighlightTextColor, uint16_t HighlightColor, uint16_t HighlightBorderColor,
 							uint16_t SelectedTextColor, uint16_t SelectedColor, uint16_t SelectBorderColor,
 							uint16_t DisableTextColor ,	uint16_t TitleTextColor, uint16_t TitleFillColor){
